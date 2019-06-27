fa738fc6d98e367758ce1021594a78a96c2cf544
aevans <aevans@starbucks.com>
2019-06-27 00:16:54 -0700

    animations added to red skull

--- haunted-mirror.js
+++ haunted-mirror.js
@@ -3,34 +3,47 @@
 Module.register("haunted-mirror", {
 
   defaults: {
-    text: "HAUNTED MEEERRRORRRR!",
+    // other default values go here and are accessible by this.config
+    text: "Haunted Mirror"
+  },
 
+  start: function () { //Setting up interval for refresh
+    // var self = this;
+    // setInterval(function () {
+    //   self.updateDom();
+    //   console.log('NEW CYCLE LOU');
+    // This seems to do well at restarting and waiting for an interval to restart again/recycle. Not necessary for animations yet, we're not getting data.
+    // }, 20000);
   },
 
   getDom: function () {
+    var self = this;
     var wrapper = document.createElement("div");
     wrapper.id = 'wrapper';
+    wrapper.style.display = "flex"
 
-    // setTimeout(function () {
-    //   console.log('timeout');
-    // }, 300000);
-
-    this.alyssa(wrapper)
-
+    self.renderContent(wrapper)
     return wrapper;
   },
 
-  alyssa: function (wrapper) {
-    console.log('alyssa!');
-    var deathWrapper = document.createElement("div");
-    deathWrapper.id = 'death-wrapper';
+  renderContent: function (wrapper) {
+    var fadeWrapper = document.createElement("div");
+    fadeWrapper.id = 'fade-wrapper';
+
+    var wiggleWrapper = document.createElement("div");
+    wiggleWrapper.id = 'wiggle-wrapper';
+
+    fadeWrapper.appendChild(wiggleWrapper);
+
+    var jumpWrapper = document.createElement("div");
+    jumpWrapper.id = 'jump-wrapper';
+    wiggleWrapper.appendChild(jumpWrapper)
     var deathPic = document.createElement("img");
     deathPic.src = "/haunted-mirror/red-death.png";
     deathPic.id = 'death-pic';
-    deathPic.class =
-      // deathPic.style.maxHeight = "500px"
-      deathWrapper.appendChild(deathPic);
-    wrapper.appendChild(deathWrapper)
+    jumpWrapper.appendChild(deathPic);
+
+    return wrapper.appendChild(fadeWrapper)
   },
 
   getStyles: function () {
