fd875c7007d3922772f8165e0102d6f277cae353
aevans <aevans@starbucks.com>
2019-06-26 22:36:33 -0700

    have basic fade in and out, need t make it loop

--- haunted-mirror.js
+++ haunted-mirror.js
@@ -1,13 +1,39 @@
+/* eslint-disable quotes */
+/* eslint-disable indent */
 Module.register("haunted-mirror", {
-  // Default module config.
+
   defaults: {
-    text: "HAUNTED MEEERRRORRRR!"
+    text: "HAUNTED MEEERRRORRRR!",
+
   },
 
-  // Override dom generator.
   getDom: function () {
     var wrapper = document.createElement("div");
-    wrapper.innerHTML = this.config.text;
+    wrapper.id = 'wrapper';
+
+    // setTimeout(function () {
+    //   console.log('timeout');
+    // }, 300000);
+
+    this.alyssa(wrapper)
+
     return wrapper;
-  }
+  },
+
+  alyssa: function (wrapper) {
+    console.log('alyssa!');
+    var deathWrapper = document.createElement("div");
+    deathWrapper.id = 'death-wrapper';
+    var deathPic = document.createElement("img");
+    deathPic.src = "/haunted-mirror/red-death.png";
+    deathPic.id = 'death-pic';
+    deathPic.class =
+      // deathPic.style.maxHeight = "500px"
+      deathWrapper.appendChild(deathPic);
+    wrapper.appendChild(deathWrapper)
+  },
+
+  getStyles: function () {
+    return [this.file("haunted-mirror.css")]
+  },
 });
