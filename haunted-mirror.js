5c3b20628a3f52474e859fc0eb26a7f52fa8bbb2
aevans <aevans@starbucks.com>
2019-06-27 23:10:45 -0700

    improvements to blood-drip and red-death

--- haunted-mirror.js
+++ haunted-mirror.js
@@ -1,34 +1,42 @@
 /* eslint-disable quotes */
 /* eslint-disable indent */
-Module.register("haunted-mirror", {
+
+// Feel free to clean up my code, it's messy in here for development reasons.
+Module.register('haunted-mirror', {
 
   defaults: {
     // other default values go here and are accessible by this.config
-    text: "Haunted Mirror",
+    text: 'Haunted Mirror',
     view: 'blood-drip'
   },
 
   start: function () { //Setting up interval for refresh
-    // var self = this;
-    // setInterval(function () {
-    //   self.updateDom();
-    //   console.log('NEW CYCLE LOU');
-    // This seems to do well at restarting and waiting for an interval to restart again/recycle. Not necessary for animations yet, we're not getting data.
-    // }, 20000);
+    var self = this;
+    let timing = 10000;
+
+    if (this.config.view === 'blood-drip') {
+      // timing = 8000
+      timing = 75000;
+    } else if (this.config.view === 'red-death') {
+      timing = 27000
+    }
+    setInterval(function () {
+      self.updateDom();
+      console.log('NEW CYCLE LOU');
+      // This seems to do well at restarting and waiting for an interval to restart again/recycle. Not necessary for animations yet, we're not getting data.
+    }, timing);
   },
 
   getDom: function () {
     var self = this;
-    var wrapper = document.createElement("div");
+    var wrapper = document.createElement('div');
     wrapper.id = 'wrapper';
-    wrapper.style.display = "flex";
-    wrapper.style.width = '100vw';
-    wrapper.style.height = '100vh';
-    console.log(this.config);
 
     if (this.config.view === 'red-death') {
       self.renderRedDeath(wrapper)
     } else if (this.config.view === 'blood-drip') {
+      self.renderBlood(wrapper)
+    } else {
       self.renderBlood(wrapper)
     }
 
@@ -36,49 +44,50 @@ Module.register("haunted-mirror", {
   },
 
   renderBlood: function (wrapper) {
-    var bloodWrapper = document.createElement("div");
+    var self = this;
+    var bloodWrapper = document.createElement('div');
     bloodWrapper.id = 'blood-wrapper';
+    bloodWrapper.classList.add('animate');
 
-
-    var stretchWrapper = document.createElement("div");
-    stretchWrapper.id = 'stretch-wrapper';
-    bloodWrapper.appendChild(stretchWrapper);
-    var bloodPic = document.createElement("img");
-    bloodPic.src = "/haunted-mirror/blood-drip.png";
+    var bloodPic = document.createElement('img');
+    bloodPic.src = '/haunted-mirror/blood-drip.png';
     bloodPic.id = 'blood-pic';
-    var redBlock = document.createElement("img");
-    redBlock.src = "/haunted-mirror/red-block.png";
+
+    var redBlock = document.createElement('img');
+    redBlock.src = '/haunted-mirror/red-block.png';
     redBlock.id = 'red-block';
 
+    // To have writing on the top of the blood by uncommenting the below code
     // var helpMe = document.createElement('p')
     // helpMe.id = 'help-me';
     // helpMe.innerHTML = 'help me...';
-    // stretchWrapper.appendChild(helpMe)
-    stretchWrapper.appendChild(redBlock)
-    stretchWrapper.appendChild(bloodPic);
-    console.log('WRAPPER: ', stretchWrapper);
+    // bloodWrapper.appendChild(helpMe)
 
-    return wrapper.appendChild(bloodWrapper)
+    // bloodWrapper.appendChild(stretchWrapper);
+    bloodWrapper.appendChild(redBlock)
+    bloodWrapper.appendChild(bloodPic);
+    wrapper.appendChild(bloodWrapper);
+    self.start()
   },
 
   renderRedDeath: function (wrapper) {
-    var fadeWrapper = document.createElement("div");
-    fadeWrapper.id = 'fade-wrapper';
+    var deathWrapper = document.createElement('div');
+    deathWrapper.id = 'death-wrapper';
 
-    var wiggleWrapper = document.createElement("div");
+    var wiggleWrapper = document.createElement('div');
     wiggleWrapper.id = 'wiggle-wrapper';
 
-    fadeWrapper.appendChild(wiggleWrapper);
-
-    var jumpWrapper = document.createElement("div");
+    var jumpWrapper = document.createElement('div');
     jumpWrapper.id = 'jump-wrapper';
-    wiggleWrapper.appendChild(jumpWrapper)
-    var deathPic = document.createElement("img");
-    deathPic.src = "/haunted-mirror/red-death.png";
-    deathPic.id = 'death-pic';
-    jumpWrapper.appendChild(deathPic);
 
-    return wrapper.appendChild(fadeWrapper)
+    var deathPic = document.createElement('img');
+    deathPic.src = '/haunted-mirror/red-death.png';
+    deathPic.id = 'death-pic';
+
+    deathWrapper.appendChild(wiggleWrapper);
+    wiggleWrapper.appendChild(jumpWrapper);
+    jumpWrapper.appendChild(deathPic);
+    return wrapper.appendChild(deathWrapper)
   },
 
   getStyles: function () {
